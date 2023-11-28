9249e51cbce505cca3d59917adac1773f163016c
Arkhrer <jonasrngl@gmail.com>
2023-11-28 20:52:03 -0300

    introduced used of file manager

--- src/Processo.cpp
+++ src/Processo.cpp
@@ -28,6 +28,44 @@ Processo *Processo::run(int cpuTime)
     std::cout << "Rodou o processo de id " << this->id << " faltam: " << this->timeLeft << " operações" << std::endl;
     // FAZER IO E ARQUIVOS AQUI
 
+    //ARQUIVOS
+
+    FileManager& fileManager = FileManager::GetInstance();
+
+    srand(time(0));
+    int instruction = rand()%4;
+
+    switch(instruction){
+    case 0:
+        int fileSize = rand() % 100;
+        std::vector<bool> file;
+        for(int i = 0; i < fileSize; ++i){
+            file.push_back(rand()%2);
+        }
+        std::pair<int, int> fileData;
+        filedata = fileManager.CreateFile(file);
+        break;
+    case 1:
+        if(fileManager.CheckIfFileExists(rand() % 100, rand % 100)){
+            std::cout << "Arquivo existe" << std::endl;
+        }
+        else{
+            std::cout << "Arquivo nao existe" << std::endl;
+        }
+        break;
+    case 2:
+        if(fileManager.DeleteFile(rand() % 100, rand % 100)){
+            std::cout << "Arquivo deletado com sucesso" << std::endl;
+        }
+        else{
+            std::cout << "Arquivo nao valido" << std::endl;
+        }
+        break;
+    case 3:
+        std::vector<bool> file = fileManager.OpenFile(rand() % 100, rand % 100);
+        break;
+    }
+
     return this;
 }
 
